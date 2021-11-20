04af21ec41248383f8291efd43e263378df2a0ef
aguspranyoto <agusprnyt@gmail.com>
2021-11-20 16:15:59 +0700

    first push

---
+++ index.js
@@ -0,0 +1,7 @@
+const nama = "Agus Pranyoto"; //const gabisa diganti
+let hobi = "Fotografi"; //let bisa diganti
+//const, let, var = tipe data / declare
+//console.log = tracing (nyari tahu suatu hal) / manggil
+hobi = "Motoran";
+console.log(`${nama} ${hobi}`);
+//cara munculin search ctrl+shift+p
